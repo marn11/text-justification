ea7e8f6e05fbd79c28cebcf8cf9eb9beb7a0c07b
marn11 <yourintv.84@gmail.com>
2026-01-11 02:08:00 +0100

    supporting plain text requests using bodyparser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //when we receive a plain text request, we parse its body
+  // as a string and put it in the req body
+  app.use(bodyParser.text({ type: 'text/plain' }));
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
